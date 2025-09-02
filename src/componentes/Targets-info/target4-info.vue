e2c7100192eb216884e8f36c82d85e5ea6a8dd9e
waldooCreator <walterestebanvelascocontreras@gmail.com>
2025-09-02 12:01:42 -0500

    capacitacion a Jair2

--- src/componentes/Targets-info/target4-info.vue
+++ src/componentes/Targets-info/target4-info.vue
@@ -1,13 +1,5 @@
 <template>
-<div
-    class="bg-[rgba(235,102,55,255)] text-white rounded-full shadow-md relative flex items-center justify-between
-      px-3 sm:px-4 md:px-8 lg:px-16
-      py-4 sm:py-2.5 md:py-3
-      w-[calc(100%-2rem)] sm:w-[calc(100%-3rem)] md:w-full
-      mx-auto md:max-w-7xl
-      mt-4 md:mt-4 mb-6 md:mb-10"
-  >
-  </div>
+
 </template>
 
 <script>
